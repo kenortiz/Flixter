ac462b67bc67b429c101e8bb982da7803a0054f0
Kenneth Ortiz <kenortiz@users.noreply.github.com>
2016-03-27 23:44:25 +0000

    Added authenticate_user! before action to lessons controller. Also, added custom before action to require enrollment of course in order to see lesson show page. If user is not enrolled they will be redirected back to course detail page with an alert saying You Are Not Enrolled In This Course.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
-  
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_course
+
   def show
   end
 
   private
+
+  def require_enrollment_for_current_course
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: 'You Are Not Enrolled In This Course'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
